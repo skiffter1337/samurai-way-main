65908bad6d3e68ef3cf41e21571be90612ee325a
Ilya <shulapov1999@gmail.com>
2023-06-15 13:19:41 +0300

    94 add lazy loading

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {Suspense} from 'react';
 import './App.css';
 import {Navbar} from "./components/Navbar/Navbar";
 import {Route} from "react-router-dom";
@@ -6,15 +6,20 @@ import {News} from "./components/News/News";
 import {Music} from "./components/Music/Music";
 import {Settings} from "./components/Settings/Settings";
 import {Friends} from "./components/Friends/Friends";
-import UsersContainer from "./components/Users/UsersContainer";
-import ProfileContainer from "./components/Profile/ProfileContainer";
-import HeaderContainer from "./components/Header/HeaderContainer";
-import DialogsContainer from "./components/Dialogs/DialogsContainer";
-import Login from "./Login/Login";
 import {connect} from "react-redux";
 import {initializeAppTC} from "./redux/reducers/appReducer";
 import {AppStoreType} from "./redux/redux-store";
 import {Preloader} from "./components/common/Preloader/Preloader";
+import HeaderContainer from "./components/Header/HeaderContainer";
+// import UsersContainer from "./components/Users/UsersContainer";
+// import ProfileContainer from "./components/Profile/ProfileContainer";
+// import DialogsContainer from "./components/Dialogs/DialogsContainer";
+// import Login from "./Login/Login";
+const Login = React.lazy(() => import ("./Login/Login"));
+const ProfileContainer = React.lazy(() => import ("./components/Profile/ProfileContainer"));
+const UsersContainer = React.lazy(() => import ("./components/Users/UsersContainer"));
+const DialogsContainer = React.lazy(() => import ("./components/Dialogs/DialogsContainer"));
+
 
 type MapStateToPropsType = {
     initialized: boolean
@@ -41,7 +46,9 @@ class App extends React.Component<MapDispatchToPropsType & MapStateToPropsType> 
             <div className="app-wrapper">
                 <HeaderContainer/>
                 <Navbar/>
+
                 <div className="app-wrapper-content">
+                    <Suspense fallback={<div><Preloader/></div>}>
                     <Route path="/dialogs" render={() => <DialogsContainer/>}/>
                     <Route path="/profile/:userId?" render={() => <ProfileContainer/>}/>
                     <Route path="/users" render={() => <UsersContainer/>}/>
@@ -51,6 +58,7 @@ class App extends React.Component<MapDispatchToPropsType & MapStateToPropsType> 
                     <Route path="/settings" render={() => Settings}/>
                     <Route path="/friends" render={() => <Friends/>}/>
                     {/*<Route path="/*" render={ () =><Error404/>}/>*/}
+                </Suspense>
                 </div>
             </div>
 
